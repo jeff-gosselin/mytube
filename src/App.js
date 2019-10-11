b99c81062d72fd69c9c906a0c059ab89ea5428c4
jeff-gosselin <jgoz.email@gmail.com>
2019-10-11 13:40:51 -0400

    able to load more videos

--- src/App.js
+++ src/App.js
@@ -3,24 +3,34 @@ import VideoItem from './components/VideoItem';
 import './App.css';
 import axios from 'axios';
 
-const API = 'AIzaSyBJkFA-f5xF04VkFGQMA-wQcl5nsIWJ4Ro';
+const API = 'AIzaSyBeimXtjgzfQcogY-fP8_CHPybmLpFaieo';
 const URL = `https://www.googleapis.com/youtube/v3/search?key=${API}&part=snippet&type=video&q=surf`;
 
 class App extends Component {
   state = {
-    ytData: []
+    ytData: [],
+    nextPage: ''
   }
   async componentDidMount() {
     let yt = await axios.get(URL);
-    
     this.setState({
       ytData: yt.data.items,
+      nextPage: yt.data.nextPageToken
+    })
+  }
+
+  async moreVideos() {
+    let yt = await axios.get(`${URL}&pageToken=${this.state.nextPage}`);
+    console.log(yt);
+    this.setState({
+      ytData: [...this.state.ytData, ...yt.data.items],
+      nextPage: yt.data.nextPageToken
     })
   }
 
   render() {
     let ytVideos;
-
+  
     if (this.state.ytData.length > 0) {
       ytVideos = this.state.ytData.map(vid => <VideoItem key={vid.id.videoId} data={vid.snippet} />);
     }
@@ -28,6 +38,9 @@ class App extends Component {
     return (
       <div className="App">
         {ytVideos}
+        <div className="more">
+          <button onClick={this.moreVideos.bind(this)}>Load More Videos</button>
+        </div>  
       </div>
     );
   } 
