64b90730dcde36c11664693d288c4f83b6e611ca
jeff-gosselin <jgoz.email@gmail.com>
2019-10-10 20:13:31 -0400

    implementing YouTube API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const API = 'AIzaSyBJkFA-f5xF04VkFGQMA-wQcl5nsIWJ4Ro';
+const URL = `https://www.googleapis.com/youtube/v3/search?key=${API}&part=snippet&type=video&q=surf`;
+
 function App() {
+  console.log(URL);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      I am creating a YouTube app.
     </div>
   );
 }
