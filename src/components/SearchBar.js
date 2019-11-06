36507c45a6eb1c11579946c53c26bfd254ba3f6a
jeff-gosselin <jgoz.email@gmail.com>
2019-11-06 18:10:17 -0500

    styling nav

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
+import '../css/SearchBar.css';
+import SurfTube from '../img/surftube.png';
 
 class SearchBar extends Component {
-    state = {
-        query: '' 
-    }
+  state = {
+    query: ''
+  };
 
-    handleInput = (e) => {
-        this.setState({
-            query: e.target.value
-        })
-    }
+  handleInput = e => {
+    this.setState({
+      query: e.target.value
+    });
+  };
 
-    render() {
-        return (
-            <form onSubmit={(e) => this.props.handleSubmit(e, this.state.query)}>
-                <input onChange={this.handleInput} type="text" />
-                <button>Search</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <div className='title-bar'>
+        <img className='surf-tube-logo' src={SurfTube} alt='' />
+        <form onSubmit={e => this.props.handleSubmit(e, this.state.query)}>
+          <input onChange={this.handleInput} type='text' />
+          <button>Search</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
