5d497fd2e33d0dc3886d5d60da22eec2b2975925
jeff-gosselin <jgoz.email@gmail.com>
2019-11-07 12:19:22 -0500

    styling

--- src/components/VideoListItem.js
+++ src/components/VideoListItem.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../css/VideoListItem.css';
 
-const VideoListItem = (props) => {
-    const {title, description, thumbnails} = props.data;
+const VideoListItem = props => {
+  const { title, description, thumbnails } = props.data;
 
-    return (
-        <div className="video-list-item">
-            <div className="video-list-item-image">
-                <Link to={`/vid/${props.videoId}`}>
-                    <img src={thumbnails.medium.url} alt="" />
-                </Link>
-            </div> 
-            <div className="video-list-item-details">
-                <Link to={`/vid/${props.videoId}`}>
-                    <h1>{title}</h1>
-                </Link>
-                <p>{description}</p>
-            </div>    
-        </div>
-    )
-}
+  return (
+    <div className='video-list-item'>
+      <div className='video-list-item-image'>
+        <Link to={`/vid/${props.videoId}`}>
+          <img src={thumbnails.medium.url} alt='' />
+        </Link>
+      </div>
+      <div className='video-list-item-details'>
+        <Link className='video-title' to={`/vid/${props.videoId}`}>
+          <h1>{title}</h1>
+        </Link>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
 
 export default VideoListItem;
